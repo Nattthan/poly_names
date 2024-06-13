552812353ad65e02c12dd048c87c67049bbd3aad
nattthan <natthan34@outlook.com>
2024-06-13 16:52:51 +0200

    add possibility to delete a game with the code

--- frontend/main.js
+++ frontend/main.js
@@ -1,27 +1,24 @@
 import { WordsService } from "./services/words-service.js";
 import { CardsService } from "./services/cards-service.js";
 import { ColorCardsService } from "./services/colorCards-service.js";
+import { GameService } from "./services/game-service.js";
 
 function run() {
   console.log("all words");
-  const wordsService = new WordsService();
-  // Correct usage if keeping findAll as a static method
+
   WordsService.findAll().then((data) => {
     console.log(data);
   });
 
   console.log("all cards");
-  const cardsService = new CardsService();
-  // Correct usage if keeping findAll as a static method
-  CardsService.displayCards();
+  CardsService.findAll();
   console.log("Displaying cards");
 
   console.log("all colored cards");
-  const colorCardsService = new ColorCardsService();
-  // Correct usage if keeping findAll as a static method
-  ColorCardsService.findAll(9994).then((data) => {
+  ColorCardsService.findAll(5958).then((data) => {
     console.log(data);
   });
+
 }
 
 window.addEventListener("load", run);
--- frontend/services/colorCards-service.js
+++ frontend/services/colorCards-service.js
@@ -1,17 +1,18 @@
 export class ColorCardsService {
-    constructor() {
-      this.cards = [];
-    }
-  
-    static async findAll(gameCode){
-      const response = await fetch(`http://localhost:8080/game/${gameCode}/colorCards`);
-  
-      if(response.status === 200){
-          const data = await response.json();
-          return data;
-      }
-  
-      return this.cards;
+  constructor() {
+    this.cards = [];
+  }
+
+  static async findAll(gameCode) {
+    const response = await fetch(
+      `http://localhost:8080/game/${gameCode}/colorCards`
+    );
+
+    if (response.status === 200) {
+      const data = await response.json();
+      return data;
     }
 
+    return this.cards;
   }
+}
